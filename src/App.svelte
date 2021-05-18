d927ff6b67be0831da23e1d551ae2ae02ee05b94
Shutto <baw.keepon69@gmail.com>
2021-05-19 02:20:18 +0900

    divide Todo component

--- src/App.svelte
+++ src/App.svelte
@@ -1,29 +1,46 @@
 <script>
-  let text = "Sevlte";
-  const handleText = (event) => (text = event.target.value);
-
-  let count = 0;
-  const handleCount = (event) => (count += 1);
-
-  let isActive = false;
-  const handleActive = (event) => (isActive = !isActive);
+  import Todo from "./components/Todo.svelte";
+  let todos = [];
+  let newId = () => Date.now().toString(36);
+  let newTodoText = "";
+  const addTodo = () => {
+    todos = [...todos, { id: newId(), text: newTodoText, done: false }];
+    newTodoText = "";
+  };
+  const handleDone = (id) => {
+    const result = todos.map((todo) => {
+      if (todo.id === id) {
+        todo.done = !todo.done;
+      }
+      return todo;
+    });
+    todos = result;
+  };
+  const deleteTodo = (id) => {
+    const result = todos.filter((todo) => todo.id !== id);
+    todos = result;
+  };
 </script>
 
-<p>Hello {text}!</p>
-
-<input type="text" bind:value={text} />
-
-<input type="text" value={text} on:input={handleText} />
-
-<input type="number" bind:value={count} />
-
-<button on:click={handleCount}>+</button>
-
-<button on:click={handleActive} class:active={isActive}>いいね</button>
+<div class="main">
+  <h1>SvelteのTODOアプリ</h1>
+  <div class="todos">
+    {#each todos as todo (todo.id)}
+      <Todo {handleDone} {deleteTodo} {todo} />
+    {/each}
+  </div>
+  <input type="text" bind:value={newTodoText} />
+  <button on:click={addTodo} disabled={newTodoText === ""}>追加</button>
+</div>
 
 <style>
-  .active {
-    background-color: #ff3e00;
-    color: white;
+  .main {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+  .todos {
+    width: 100%;
+    margin: 0 auto;
   }
 </style>
---
+++ src/components/Todo.svelte
@@ -0,0 +1,37 @@
+<script>
+  export let todo;
+  export let handleDone;
+  export let deleteTodo;
+</script>
+
+<div class="todo">
+  <p class={todo.done ? "done" : ""}>{todo.text}</p>
+  <div>
+    <button on:click={handleDone(todo.id)}>{todo.done ? "解除" : "完了"}</button
+    >
+    <button on:click={deleteTodo(todo.id)} class="delete">削除</button>
+  </div>
+</div>
+
+<style>
+  .todo {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    width: 70%;
+    padding: 0.4rem 2rem;
+    margin: 1rem auto;
+    border-radius: 15px;
+    background-color: #ffffff;
+  }
+  .todo p,
+  .todo button {
+    margin: 5px 10px;
+  }
+  .done {
+    text-decoration: line-through;
+  }
+  .delete {
+    background-color: red;
+  }
+</style>
